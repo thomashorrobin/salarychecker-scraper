74c804caded3b69a25ec637f0fb1f684666bdaa3
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2018-07-12 11:34:41 +0100

    added some hopfully useful comments

--- crawler.go
+++ crawler.go
@@ -21,8 +21,14 @@ func StartCrawl(u url.URL) PageChannel {
 	finishedPageChannel = make(chan page.Page)
 	checkedURLs = make(map[url.URL]bool)
 	mux = sync.Mutex{}
+
+	// start listing to urlCheckingChannel
 	go listenOnURLCheckingChannel()
+
+	// put the initial url on the channel
 	urlCheckingChannel <- u
+
+	// we return finishedPageChannel to the main function so that is can listen there for parsed pages that have been completed
 	return finishedPageChannel
 }
 
@@ -30,6 +36,7 @@ func markAsProcessedThenCheckIfCrawlIsDone(url url.URL) {
 	mux.Lock()
 	checkedURLs[url] = true
 	if haveAllQueuedURLsBeenProcessed(checkedURLs) {
+		// if all urls are marked as processed then we close the channel to allow the program to exit
 		close(finishedPageChannel)
 	}
 	mux.Unlock()
@@ -38,6 +45,7 @@ func markAsProcessedThenCheckIfCrawlIsDone(url url.URL) {
 func processURL(url url.URL) {
 	page := page.ParseURL(url)
 	for _, link := range page.GetLinks() {
+		// once a page is returned put all the links on urlCheckingChannel
 		urlCheckingChannel <- link
 	}
 	finishedPageChannel <- page
@@ -45,9 +53,11 @@ func processURL(url url.URL) {
 	go markAsProcessedThenCheckIfCrawlIsDone(url)
 }
 
-func checkURL(u url.URL) {
+// this method checks to see if the url is being processed/ has been processed and if it hasn't it'll set off a go routine to process it
+func checkThenProcessURL(u url.URL) {
 	mux.Lock()
 	_, success := checkedURLs[u]
+	// if the url isn't in the map then the program hasn't seen it before
 	if !success {
 		checkedURLs[u] = false
 		go processURL(u)
@@ -57,7 +67,7 @@ func checkURL(u url.URL) {
 
 func listenOnURLCheckingChannel() {
 	for elem := range urlCheckingChannel {
-		go checkURL(elem)
+		go checkThenProcessURL(elem)
 	}
 }
 
