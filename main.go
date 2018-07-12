51c1ecf24b00c3cdd45f2ef5421381c624b1b3f7
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2018-07-12 11:17:09 +0100

    added command line option

--- main.go
+++ main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"net/url"
+	"os"
 )
 
 func main() {
-	url, _ := url.Parse("https://monzo.com")
-	// page := parseURL(*url)
-	// page.PrintPage()
+	var url url.URL
+	if len(os.Args) >= 2 {
+		urlFlag, _ := url.Parse(os.Args[1])
+		url = *urlFlag
+	} else {
+		urlDefault, _ := url.Parse("https://monzo.com")
+		url = *urlDefault
+	}
 	b := Benchmark{}
 	b.Start()
-	c := StartCrawl(*url)
+	c := StartCrawl(url)
 	// go c.checkURL(*url)
 	for elem := range c {
 		elem.PrintPage()
